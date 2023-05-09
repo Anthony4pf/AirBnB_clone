3965f4c49c19b213631934d194018be130396e85
Anthony Ede <edeanthony14@gmail.com>
2023-05-09 15:24:03 -0700

    Add BaseModel

---
+++ models/base_model.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env Python3
+"""This module contains the BaseModel Class"""
+import uuid
+from datetime import datetime
+
+
+class BaseModel():
+    """Base Class that define common attributes/methods for other classes"""
+
+    def __init__(self):
+        """Class Constructor"""
+
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """String Representation of the Instance"""
+        return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """Update the public attr updated_at with the current datetime"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Returns a dict containing keys/values of __dict__ of the instance"""
+        new_dict = {**self.__dict__}
+        new_dict.update({
+                        "__class__": type(self).__name__,
+                        "created_at": self.created_at.isoformat(),
+                        "updated_at": self.updated_at.isoformat()})
+
+        return new_dict
